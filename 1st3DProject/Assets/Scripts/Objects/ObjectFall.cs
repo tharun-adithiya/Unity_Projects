30d602bea76f05ca698e4db5793784b7f1023bc3
tharun-adithiya <tharunadithiyav.s@gmail.com>
2025-06-10 01:06:10 +0530

    Latest version of the objects scripts are updated

--- 1st3DProject/Assets/Scripts/Objects/ObjectFall.cs
+++ 1st3DProject/Assets/Scripts/Objects/ObjectFall.cs
@@ -4,42 +4,34 @@ using UnityEngine;
 public class ObjectFall : MonoBehaviour
 {
     private Transform _targetTransform;
-    [SerializeField]private GameObject _fallingObject;
-    [SerializeField]Rigidbody _ballRb;
-    [SerializeField] private float _fallSpeed;
-    private Vector3 _objectMotion;
-    [SerializeField] private Transform _spawnArea;
-    public ObjectMovement movement;
+    //public ObjectMovement movement;
     public GridGenerator m_generator;
     private Vector3 randomPosition;
     [SerializeField]private float m_spawnRate=0.5f;
     [SerializeField] private float m_spawnHeight = 25f;
+    private ObjectPooler m_pooler;
 
 
     void Start()
     {
-        
         _targetTransform=GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-
+        m_pooler = ObjectPooler.instance;
         StartCoroutine(SpawnObstacle(m_spawnRate));
     }
 
-    private void FixedUpdate()
-    {
-        
-    }
-
-    void Update()
+    void FixedUpdate()
     {
         randomPosition = new Vector3(m_generator.randomPosX, m_spawnHeight, m_generator.randomPosZ);
-
     }
     private IEnumerator SpawnObstacle(float spawnRate)
     {
         while (_targetTransform != null)
         {
             yield return new WaitForSeconds(spawnRate);
-            Instantiate(_fallingObject, randomPosition, _fallingObject.transform.rotation);
+            
+            m_pooler.SpawnFromPool("FallingSphere",randomPosition,Quaternion.identity);
+            ObjectMovement movement =m_pooler.GetComponent<ObjectMovement>();
+           // movement.Initialize(GameObject.Find("Player").transform);
             //Debug.Log("Xpos:" + randomPosition.x + ", Zpos:" + randomPosition.z);
         }
         
--- 1st3DProject/Assets/Scripts/Objects/ObjectMovement.cs
+++ 1st3DProject/Assets/Scripts/Objects/ObjectMovement.cs
@@ -2,54 +2,46 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ObjectMovement : MonoBehaviour
+public class ObjectMovement : MonoBehaviour,IPooledObject
 {
     private Vector3 _ballMotion;
     public GameObject ball;
-    [SerializeField]private Transform _target;
+    [HideInInspector]private Transform m_target;
     [SerializeField]private float _fallSpeed=5f;
     private Rigidbody _rb;
     private bool m_isStopFollowing=false;
-    public Material ballMaterial;
-   // [SerializeField] private float inAirTimer = 2f;   For Later Use
-    Color ballAlpha;
     [SerializeField] private LayerMask m_groundLayer;
     private float m_groundCheckRadius=0.5f;
     [SerializeField] private Transform groundChecker;
-    
+    public static float damage = 25f;
 
-    //private bool m_isAirTimeOver=false; For later Use
+    
    
     void Start()
     {
-        _target=GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        _rb=GetComponent<Rigidbody>();
-        ballAlpha=ballMaterial.color;
-       // ballAlpha.a = 50;
+        m_target = GameObject.Find("Player").transform;
+        if (m_target != null)
+        {
+            Debug.Log("Target  found");
+        }
+        _rb =GetComponent<Rigidbody>();
+
     }
 
-    
+    public void Initialize(Transform target)
+    {
+        m_target = target;
+        m_isStopFollowing = false;
+        Debug.Log("Object initialized with target: " + target?.name);
+    }
     void Update()
     {
-        
-        if (_target == null)
-        {
-            Debug.Log("Object not found");
-        }
-
        
-        /*if (!isGrounded())        For later use
-        {
-            Debug.Log("Object not in ground");
-            StartCoroutine(CheckDistance());
-            
-        }*/
-
         if (!m_isStopFollowing)
         {
-            ObjectBehavior(); 
+            OnSpawnObject();
+            m_isStopFollowing = false;
         }
-        
 
     }
     private void OnCollisionEnter(Collision collision)
@@ -57,48 +49,31 @@ public class ObjectMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             print("ObjectCollided");
-            Destroy(gameObject);
+            m_isStopFollowing = true;
         }
         if (collision.gameObject.CompareTag("Ground"))
         {
+           
             m_isStopFollowing = true;
-            //StartCoroutine(BallDisappear());
+            
             //Destroy(gameObject);
         }
     }
 
-    private void ObjectBehavior()
+    public void OnSpawnObject()
     {
-        transform.position = Vector3.Lerp(transform.position, _target.position, _fallSpeed * Time.deltaTime);
+        m_target = GameObject.Find("Player").transform;
+
+        if (m_target == null)
+        {
+            Debug.LogWarning("Target not found");
+        }
+        transform.position = Vector3.Lerp(transform.position, m_target.position, _fallSpeed * Time.deltaTime);
     }
 
-    /*private IEnumerator CheckDistance()           For later use
-    {
-        Debug.Log("Hello from Coroutine");
-        
-        Debug.Log(m_isAirTimeOver);
-        yield return new WaitForSeconds(inAirTimer);
-        if (m_isAirTimeOver == false)
-        {
-            m_isAirTimeOver = true;
-        }
-        
-    }*/
     private bool isGrounded()
     {
         return Physics.CheckSphere(groundChecker.position, m_groundCheckRadius, m_groundLayer);
     }
-    /*private IEnumerator BallDisappear()    For later use
-    {
 
-        for (int i = 0; i < 3; i++)
-        {
-            ballAlpha.a = 50;
-            
-            ballAlpha.a = 100;
-            
-        }
-        yield return null;
-        
-    }*/
 }
