adc621ee931d33f3e06e93b36a4e52c8f52ed893
tharun-adithiya <tharunadithiyav.s@gmail.com>
2025-07-30 22:19:46 +0530

    Audio manager is modified to hold different audio. AudioSwitcherHandles music switching

--- 1st3DProject/Assets/Scripts/Audio/AudioManager.cs
+++ 1st3DProject/Assets/Scripts/Audio/AudioManager.cs
@@ -1,14 +1,13 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    public List<AudioClip> audioList = new List<AudioClip>();
     
-    [SerializeField] private List<AudioClip> m_audioList = new ();
-    //[SerializeField] private List<compon>
-    void Start()
-    {
-        
-    }
+    [HideInInspector] public Dictionary<string, AudioClip> m_audioDictionary = new Dictionary<string, AudioClip> ();
+
 
 }
