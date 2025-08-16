033473a4fbc7099e51bd6c54da765ebce76bd044
tharun-adithiya <tharunadithiyav.s@gmail.com>
2025-08-16 09:36:31 +0530

    Bug fixed in object pooler. Remaining object related scripts are modifier

--- 1st3DProject/Assets/Scripts/Objects/ObjectsPoolScripts/ObjectPooler.cs
+++ 1st3DProject/Assets/Scripts/Objects/ObjectsPoolScripts/ObjectPooler.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
@@ -9,15 +8,15 @@ public class ObjectPooler : MonoBehaviour
     public Transform m_targetToFollow;
     [HideInInspector] public static ObjectPooler pooler;
     [SerializeField] private AsteroidRainStarter m_rainStarter;
-    private Queue<GameObject> m_objectPool = new Queue<GameObject>();
+
     [System.Serializable]
     public class Pool
     {
         public string objectName;
         public GameObject prefab;
         public int poolSize;
-
     }
+
     #region Singleton
     public static ObjectPooler instance;
     private void Awake()
@@ -26,52 +25,49 @@ public class ObjectPooler : MonoBehaviour
     }
     #endregion
 
-
     [HideInInspector] public Dictionary<string, Queue<GameObject>> objectPoolDictionary;
-    [SerializeField]private List<Pool> pools;
+    [SerializeField] private List<Pool> pools;
+
     void Start()
     {
-        
         objectPoolDictionary = new Dictionary<string, Queue<GameObject>>();
 
         foreach (var pool in pools)
         {
-            //Queue<GameObject> objectPool = new Queue<GameObject>();
+            Queue<GameObject> objectPool = new Queue<GameObject>(); // Fixed: new queue per pool
+
             for (int i = 0; i < pool.poolSize; i++)
             {
                 GameObject poolObj = Instantiate(pool.prefab);
                 poolObj.SetActive(false);
-                
-                m_objectPool.Enqueue(poolObj);
+                objectPool.Enqueue(poolObj);
             }
-            objectPoolDictionary.Add(pool.objectName, m_objectPool);
+
+            objectPoolDictionary.Add(pool.objectName, objectPool);
         }
     }
+
     public GameObject SpawnFromPool(string objName, Vector3 position, quaternion rotation)
     {
-        
         if (!objectPoolDictionary.ContainsKey(objName))
         {
             Debug.LogWarning($"Object with name {objName} is not found");
+            return null;
         }
 
-        GameObject objToSpawn=objectPoolDictionary[objName].Dequeue();
-
+        GameObject objToSpawn = objectPoolDictionary[objName].Dequeue();
         objToSpawn.SetActive(true);
-
         objToSpawn.transform.SetPositionAndRotation(position, rotation);
-        IPooledObject pooledObj=objToSpawn.GetComponent<IPooledObject>();
+
+        IPooledObject pooledObj = objToSpawn.GetComponent<IPooledObject>();
         if (pooledObj != null)
         {
-            Debug.Log(m_targetToFollow);
-            //movement.Initialize(m_targetToFollow);
             pooledObj.Initialize(m_targetToFollow);
-
-             pooledObj.OnSpawnObject();
-
+            pooledObj.OnSpawnObject();
         }
+
         objectPoolDictionary[objName].Enqueue(objToSpawn);
+
         return objToSpawn;
     }
-
 }
