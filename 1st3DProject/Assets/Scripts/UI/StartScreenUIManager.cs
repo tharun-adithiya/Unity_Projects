4929f9ea2741675a90e34e7b736f8b39bcf0f7e8
tharun-adithiya <tharunadithiyav.s@gmail.com>
2025-08-16 10:00:40 +0530

    UI manager is updated. Deleted GameOverScene.cs

--- 1st3DProject/Assets/Scripts/UI/StartScreenUIManager.cs
+++ 1st3DProject/Assets/Scripts/UI/StartScreenUIManager.cs
@@ -4,18 +4,30 @@ using UnityEngine.SceneManagement;
 
 public class StartScreenUIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject m_optionsPanel;
+    [SerializeField] private GameObject m_controlsPanel;
+    private void Update()
+    {
+        if (m_controlsPanel.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnClickBack();
+        }
+    }
     public void OnClickStart()
     {
         SceneManager.LoadScene(1);
     }
     public void OnClickQuit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+       // UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
     public void OnClickOptions()
     {
-        
+        m_controlsPanel.SetActive(true);
     }
+    public void OnClickBack()
+    {
+        m_controlsPanel.SetActive(false);
+    }
+
 }
